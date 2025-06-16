514bff02ebcdd6a84b6def264e0a2a88c5f76b79
lalobgc <lalobg.c@gmail.com>
2025-06-16 17:15:13 -0600

    lesson 1.4 completed

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+try:
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents=sys.argv[1])
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+except Exception as e:
+    print(f"no argument given: {e}")
+    exit(1)

ee61072ad7a35b1eb5649ee71da98346dcd1e10c
lalobgc <lalobg.c@gmail.com>
2025-06-16 18:18:34 -0600

    Using an if instead of a try-catch block

--- main.py
+++ main.py
@@ -2,18 +2,28 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+    load_dotenv()
 
-client = genai.Client(api_key=api_key)
+    if len(sys.argv) == 1:
+        print("No argument given...")
+        sys.exit(1)
 
-try:
+    user_prompt = sys.argv[1]
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=sys.argv[1])
+        model="gemini-2.0-flash-001", contents=messages)
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-except Exception as e:
-    print(f"no argument given: {e}")
-    exit(1)
+
+if __name__ == "__main__":
+    main()

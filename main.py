258f3b5ab4bcac6251c5cb29c9cc9d9937bdf040
lalobgc <lalobg.c@gmail.com>
2025-06-16 19:57:08 -0600

    lesson 6 completed, this is my solution, somewhat artesanal

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+
 def main():
     load_dotenv()
 
@@ -12,6 +13,7 @@ def main():
         sys.exit(1)
 
     user_prompt = sys.argv[1]
+    user_flag = sys.argv[2] if len(sys.argv) == 3 else False
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
@@ -22,8 +24,12 @@ def main():
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", contents=messages)
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    if user_flag == "--verbose":
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 
 if __name__ == "__main__":
     main()

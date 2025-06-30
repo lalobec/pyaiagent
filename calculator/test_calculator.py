45d2cc9669debafc2c4a60e4fb040feeb7682b38
lalobgc <lalobg.c@gmail.com>
2025-06-30 14:04:32 -0600

    All steps are done

---
+++ calculator/test_calculator.py
@@ -0,0 +1,7 @@
+# test_calculator.py
+
+from pkg.calculator import Calculator
+
+calculator = Calculator()
+result = calculator.evaluate("3 + 7 * 2")
+print(result)
--- main.py
+++ main.py
@@ -26,7 +26,18 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages, verbose)
+    for i in range(20):
+        if i == 19:
+            print("Maximum iterations reached, exiting the program...")
+            sys.exit(1)
+
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                print(final_response)
+                break
+        except Exception as e:
+            print(f'Error in generate_content function: {e}')
 
 
 def generate_content(client, messages, verbose):
@@ -38,23 +49,45 @@ def generate_content(client, messages, verbose):
             system_instruction=system_prompt),
     )
 
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens:\
+        {response.usage_metadata.candidates_token_count}")
+
+    if response.candidates:
+        for response_candidate in response.candidates:
+            # print("respp")
+            # print(response_candidate.content)
+            # print(f'number of messages {len(messages)}')
+            messages.append(response_candidate.content)
+            # print(f'number of messages {len(messages)}')
+
     if not response.function_calls:
         return response.text
 
+    function_responses = []
     for function_call_part in response.function_calls:
         # print(f"Calling function: {function_call_part.name}"
         #      f"({function_call_part.args})")
         function_call_result = call_function(function_call_part, verbose)
+        # print("The second step for adding the types.Content from the call")
+        # print(type(function_call_result))
+        if (
+                not function_call_result.parts
+                or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
         try:
             if verbose:
                 print(f"-> {function_call_result.parts[0].function_response.response}")
         except Exception as e:
             raise f'whhaaaaat is going ooonnn...!!!! {e} !!'
+        function_responses.append(function_call_result.parts[0])
 
-    if verbose:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens:\
-        {response.usage_metadata.candidates_token_count}")
+    if not function_responses:
+        raise Exception("no function responses generated, exiting the program")
+
+    messages.append(types.Content(role="tool", parts=function_responses))
 
 
 if __name__ == "__main__":
